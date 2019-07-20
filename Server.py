89651ae2f31e5b28e4286c4e93b57ba05c2576aa
Victor Colomb <vic.col@hotmail.fr>
2019-07-20 16:16:33 +0200

    First upload

---
+++ Server.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version)
+sys.path.append('/Users/jackr/Anaconda3/Lib')
+
+
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
