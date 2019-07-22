5db5bd6e7a66dc3b9d68a8e836d32aa7c63464e1
Garfield <jack.royer.23@gmail.com>
2019-07-22 11:31:22 -0400

    download button partial js integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 function MovieOverlayOn(title) {
   document.getElementById("overlayTitle").innerHTML = title;
   var vidPath = 'videos/movies/'+title+'/vid.mpd';
+  var dwdpath = 'videos/movies/'+title+'/vid.mp4';
   document.getElementById("videoPlayer").src = vidPath;
   document.getElementById("showEpSelector").style.display = "none";
   document.getElementById("videoPlayer").style.display="block";
   document.getElementById("movieOverlay").style.display = "block";
+  document.getElementById("movieOverlayDownloadButton").href=dwdpath;
+  document.getElementById("movieOverlayDownloadButton").download=title;
 }
 function ShowOverlayOn(title) {
   document.getElementById("overlayTitle").innerHTML = title;
