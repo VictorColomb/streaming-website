cf1b0a96a47cfb20efaf286f86b8e81a81e442a6
Victor Colomb <vic.col@hotmail.fr>
2019-07-23 00:11:35 +0200

    Modified mp4ToMpdThroughFragment.py. It now accepts .mkv files

--- mp4ToMpdThroughFragment.py
+++ mp4ToMpdThroughFragment.py
@@ -4,6 +4,9 @@ import copy
 import subprocess
 import shutil
 
+# GRABS .mp4 OR .mkv FILES (preferably called E??.mp4/.mkv) AND TRANSFORMS THEM INTO DASH READY FILES, ORGANISED INTO vid?? DIRECTORIES (for the dash) AND E?? DIRECTORIES (for the .mp4)
+# REQUIRES ffmpeg AND bento4 UTILITIES TO BE ACCESSIBLE EVERYWHERE ON THE DEVICE !!!!!!!!!!!!!
+
 cur=p.Path.cwd()
 pp=[x for x in cur.iterdir()]
 ppp=[x for x in pp if not(x.is_dir())]
@@ -15,14 +18,26 @@ for j in range(len(PP)):
             k-=1
         PP[j]=PP[j][k+1:]
 PPP=[x.split('.')[0] for x in PP]
+PPp=[x.split('.')[-1] for x in PP]
 PPPP=[x+'-frag.mp4' for x in PPP]
 
 # Fragment each media file to file-frag.mp4 AND move original media files to /E?
-for i in range(len(PP)):
+i=0
+while i<len(PP):
     shit=PPP[i][1:-9]
     os.system('mkdir '+PPP[i])
-    subprocess.call(['mp4fragment',PP[i],PPPP[i]])
-    shutil.move(PP[i],PPP[i])
+    if PPp[i]=='mkv':
+        subprocess.call(['ffmpeg','-i',PP[i],'-codec','copy',PPP[i]+'.mp4'])
+        PP[i]=PPP[i]+'.mp4'
+    if PPp[i] in ['mkv','mp4']:
+        subprocess.call(['mp4fragment',PP[i],PPPP[i]])
+        shutil.move(PP[i],PPP[i])
+        i+=1
+    else:
+        del PP[i]
+        del PPP[i]
+        del PPp[i]
+        del PPPP[i]
 
 # Partition each file-frag.mp4 using mp4dash
 for i in range(len(PP)):
