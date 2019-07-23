57fa9a21c0039030e0aec21cd7765b426a94730e
Victor Colomb <vic.col@hotmail.fr>
2019-07-23 11:45:04 +0200

    Changed listGen.py for cookins integration

--- listGen.py
+++ listGen.py
@@ -21,7 +21,7 @@ for i in range(len(pp)):
         k+=1
     pp=pp[:k]+[pp[i]]+pp[k:i]+pp[i+1:]
 for e in pp:
-    print('<div class=\"movieItem\" onclick=\"MovieOverlayOn(\'',e,'\')\"><div class=\"movie_img_wrap\"><img src=\"videos/movies/',e,'/thumb.jpg\" width=\"100%\"><p class=\"movie_image_description\">',e,'</p></div></div>',sep='',end='',file=moviesOutputTxt)
+    print('<div class=\"movieItem\" onclick=\"MovieOverlayOn(\'',e,'\');iwatched(\'',e.replace(' ','_'),'\')\"><div class=\"movie_img_wrap\"><img src=\"videos/movies/',e,'/thumb.jpg\" width=\"100%\"><p class=\"movie_image_description\">',e,'</p></div></div>',sep='',end='',file=moviesOutputTxt)
 os.chdir('..')
 
 # TV SHOWS
@@ -40,6 +40,6 @@ for i in range(len(pp)):
         k+=1
     pp=pp[:k]+[pp[i]]+pp[k:i]+pp[i+1:]
 for e in pp:
-    print('<div class=\"showItem\" onclick=\"ShowOverlayOn(\'',e,'\')\"><div class=\"tv_img_wrap\"><img src=\"videos/shows/',e,'/thumb.jpg\"><p class=\"tv_image_description\">',e,'</p></div></div>',sep='',end='',file=showsOutputTxt)
+    print('<div class=\"showItem\" onclick=\"ShowOverlayOn(\'',e,'\');iwatched(\'',e.replace(' ','_'),'\')\"><div class=\"tv_img_wrap\"><img src=\"videos/shows/',e,'/thumb.jpg\"><p class=\"tv_image_description\">',e,'</p></div></div>',sep='',end='',file=showsOutputTxt)
 moviesOutputTxt.close()
 showsOuptutTxt.close()
